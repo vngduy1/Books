8ad3ff243f8cdd2b0efc79da3384f9e3b2c47a42
Duy <duyvu372001@gmail.com>
2023-12-26 23:39:19 +0900

    sortDelete

--- src/routes/booksRoute.js
+++ src/routes/booksRoute.js
@@ -7,7 +7,9 @@ route.get('/create', booksController.create)
 route.post('/store', booksController.store)
 route.get('/:id/edit', booksController.edit)
 route.put('/:id', booksController.update)
+route.patch('/:id/restore', booksController.restore)
 route.delete('/:id', booksController.destroy)
+route.delete('/:id/force', booksController.forceDestroy)
 route.get('/:slug', booksController.show)
 
 module.exports = route
--- src/routes/meRoute.js
+++ src/routes/meRoute.js
@@ -4,5 +4,6 @@ const route = express.Router()
 const meController = require('../app/controllers/meController')
 
 route.get('/stored/books', meController.storedBooks)
+route.get('/trash/books', meController.trashBooks)
 
 module.exports = route
